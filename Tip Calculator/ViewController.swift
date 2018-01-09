814c3d3c84c09735c9002d3da3166fb245cd9742
Computer Science Workplace <ComputerScienceWorkplace@administrators-MacBook-Pro-5.local>
2018-01-08 16:31:26 -0800

    initial commit

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billLabel: UILabel!
+    @IBOutlet weak var sliderLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,36 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func numbers(_ sender: UIButton) {
+        billLabel?.text = (billLabel?.text!)! + String(sender.tag-1)
+    }
+    
+    @IBAction func resetText(_ sender: UIButton) {
+        if billLabel?.text != nil
+        {
+            billLabel?.text = ""
+        }
+    }
+    
+    @IBAction func decimalButton(_ sender: UIButton) {
+        billLabel?.text = (billLabel?.text!)! + String(".")
+    }
+    
+    @IBAction func tipSlider(_ sender: UISlider) {
+        sliderLabel?.text = String(sender.value)
+        let tipPercent = sender.value/100
+        
+        if billLabel?.text != nil
+        {
+            let billNum = Float(billLabel.text!) ?? 0
+            let tipNum = billNum*tipPercent
+            tipLabel?.text = String(tipNum)
+            totalLabel.text = String(billNum+tipNum)
+        }
+        
+    }
+    
 
 }
 
